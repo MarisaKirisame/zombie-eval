8cf107f7b5e1cdbdfe3c0b7c7984e016a3fd338a
Marisa Kirisame <marisa@cs.utah.edu>
2023-03-24 20:36:02 -0600

    save

--- python/national_geography.py
+++ python/national_geography.py
@@ -10,6 +10,13 @@ programs = ['(elsamuko-national-geographic-batch "picture.jpeg" 60 1 60 25 0.4 1
 
 run("cp picture/picture.jpeg ./")
 
-run(f"_build/bin/gimp-2.10 -i " + " ".join(["-b " + "\'" + x + "\'" for x in programs]) + "|| true")
+gimp_program = f"_build/bin/gimp-2.10 -i " + " ".join(["-b " + "\'" + x + "\'" for x in programs])
+
+DEBUG = False
+
+if DEBUG:
+    gimp_program = f"gdb --args {gimp_program}"
+
+run(gimp_program + "|| true")
 
 run("rm picture.jpeg")
