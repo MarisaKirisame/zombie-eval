431df72dd09efde0e9d20e057972425097143c69
Marisa Kirisame <marisa@cs.utah.edu>
2024-02-13 14:01:05 -0700

    save

--- build.py
+++ build.py
@@ -1,1 +1,9 @@
+import subprocess
+
+subprocess.run("git clone git@github.com:MarisaKirisame/zombie.git", shell=True, check=False)
+subprocess.run("cd zombie", shell=True, check=True)
+subprocess.run("mkdir build", shell=True, check=False)
+subprocess.run("cmake ../..", shell=True, check=True)
+subprocess.run("make", shell=True, check=True)
+
 print("hello world")
