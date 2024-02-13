0a4b19ca106df0084af9bcf02426099ba1850009
Marisa Kirisame <marisa@cs.utah.edu>
2024-02-13 15:30:19 -0700

    save

--- build.py
+++ build.py
@@ -10,8 +10,6 @@ def run_ok(cmd):
 os.environ["PATH"] = os.environ["HOME"] + "/.local/share/coursier/bin:" + os.environ["PATH"]
 os.environ["PATH"] = os.environ["HOME"] + "/.cache/coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.22%252B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.22_7.tar.gz/jdk-11.0.22+7/bin:" + os.environ["PATH"]
 os.environ["JAVA_HOME"] = os.environ["HOME"] + "/.cache/coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.22%252B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.22_7.tar.gz/jdk-11.0.22+7"
-run("cat ~/.profile")
-print(os.environ["PATH"])
 
 if not run_ok("mill -v"):
     run("""curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs""")
@@ -32,4 +30,5 @@ run("git clone git@github.com:MarisaKirisame/TVirus.git || true")
 os.chdir("TVirus")
 run("git pull")
 
-raise
+run("python3 drive.py")
+run("python3 anal.py nightly")
