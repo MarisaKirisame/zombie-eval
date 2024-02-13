264ce2ae310a0e931b1d0c5807ce0994eedff942
Marisa Kirisame <marisa@cs.utah.edu>
2024-02-13 14:08:42 -0700

    save

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ subprocess.run("ls", shell=True, check=True)
 subprocess.run("git clone git@github.com:MarisaKirisame/zombie.git", shell=True, check=False)
 os.chdir("zombie")
 subprocess.run("mkdir build", shell=True, check=False)
+os.chdir("build")
 subprocess.run("cmake ../..", shell=True, check=True)
 subprocess.run("make", shell=True, check=True)
 
