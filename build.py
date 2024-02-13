63ba7919fb39cb33698718a60a145540c613081a
Marisa Kirisame <marisa@cs.utah.edu>
2024-02-13 14:31:43 -0700

    save

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-subprocess.run("""curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs""". shell=True, check=True)
+subprocess.run("""curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs""", shell=True, check=True)
 
 subprocess.run("ls", shell=True, check=True)
 subprocess.run("git clone git@github.com:MarisaKirisame/zombie.git", shell=True, check=False)
@@ -17,4 +17,4 @@ subprocess.run("git clone git@github.com:MarisaKirisame/TVirus.git", shell=True,
 os.chdir("TVirus")
 subprocess.run("git pull", shell=True, check=False)
 
-print("hello world")
+raise
