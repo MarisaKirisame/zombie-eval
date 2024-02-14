97b94ca8f1e4a9d0c3b3a0472d511deba9c832bf
Marisa Kirisame <marisa@cs.utah.edu>
2024-02-13 23:41:18 -0700

    save

--- build.py
+++ build.py
@@ -10,6 +10,8 @@ def run_ok(cmd):
 os.environ["PATH"] = os.environ["HOME"] + "/.local/share/coursier/bin:" + os.environ["PATH"]
 os.environ["PATH"] = os.environ["HOME"] + "/.cache/coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.22%252B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.22_7.tar.gz/jdk-11.0.22+7/bin:" + os.environ["PATH"]
 os.environ["JAVA_HOME"] = os.environ["HOME"] + "/.cache/coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.22%252B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.22_7.tar.gz/jdk-11.0.22+7"
+os.environ["CPLUS_INCLUDE_PATH"] = os.getcwd() + "/nlohmann/single_include:" os.environ["CPLUS_INCLUDE_PATH"]
+print(os.environ["CPLUS_INCLUDE_PATH"])
 
 if not run_ok("mill -v"):
     run("""curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs""")
